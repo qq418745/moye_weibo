4a8d64eec6253ea77a9f1f901aa8881d9c0ab49a
考拉 <work@liuchengming.cn>
2021-03-23 14:25:55 +0800

    add

---
+++ src/main/java/didigugu/top/DiguApplication.java
@@ -0,0 +1,13 @@
+package didigugu.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/didigugu/top/DiguApplicationTests.java
@@ -0,0 +1,13 @@
+package didigugu.top;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
