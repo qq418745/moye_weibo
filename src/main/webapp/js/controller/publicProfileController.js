d9fe44306c57f4eea2a51b8074534611a17216f9
考拉 <work@liuchengming.cn>
2021-04-19 14:53:21 +0800

    debug

--- src/main/webapp/js/controller/publicProfileController.js
+++ src/main/webapp/js/controller/publicProfileController.js
@@ -71,6 +71,74 @@ app.controller('publicProfileController' ,function($scope,$controller,httpServic
     }
     $scope.init();
 
+    $scope.search = function (){
+        $scope.init();
+    }
+    /**
+     * 发评论
+     * @param commentText
+     * @param weibo
+     */
+    $scope.sendComment = function (commentText,weibo) {
+        httpService.postJson(  "../comment/add?commentText="+commentText+"&weiboId="+ weibo.weiboId, {}) .success(function (response){
+            $scope.search()
+        })
+    };
+
+    /**
+     * 添加微博收藏
+     * @param weibo
+     */
+    $scope.addFavorite = function (weibo) {
+        httpService.postJson( "../favorite/add", weibo) .success(function (commonResult){
+            if(commonResult.code ===  $scope.SUCCESS_CODE){
+                $scope.search()
+            }
+        })
+    };
+    /**
+     * 取消微博收藏
+     * @param weibo
+     */
+    $scope.deleteFavorite = function (weibo) {
+        httpService.postJson( "../favorite/unAdd", weibo) .success(function (commonResult){
+            if(commonResult.code ===  $scope.SUCCESS_CODE){
+                $scope.search()
+            }
+        })
+    };
+
+    /**
+     * 点赞微博
+     * @param weibo
+     */
+    $scope.addLike = function (weibo) {
+        httpService.postJson( "../like/add", weibo) .success(function (commonResult){
+            if(commonResult.code ===  $scope.SUCCESS_CODE){
+                $scope.search()
+            }
+        })
+    };
+
+    /**
+     * 取消点赞
+     * @param weibo
+     */
+    $scope.deleteLike = function (weibo) {
+        httpService.postJson( "../like/unAdd", weibo) .success(function (commonResult){
+            if(commonResult.code ===  $scope.SUCCESS_CODE){
+                $scope.search()
+            }
+        })
+    };
+    /**
+     * 去用户主页
+     * @param user
+     */
+    $scope.toFrofile = function (user) {
+        location.href = "./#publicProfile?userId="+ user.userId;
+    };
+
 
 
 
